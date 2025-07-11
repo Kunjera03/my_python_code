f62eb26815777ad7b6e94479d9269019b53aaef5
Kunjera03 <skkunjera004@gmail.com>
2025-07-11 14:20:00 +0530

    Update file.py

--- file.py
+++ file.py
@@ -12,12 +12,11 @@ else:
 
 #Code2
 def check():
-    if(1 < 7):
-        return True
-    return False
+    if(n <= 1):
+      return False
 
-    for i in range(2, n-1):
-        if(i % i):
+    for i in range(2, n):
+        if(n % i == 0):
             return False 
         return True 
 
