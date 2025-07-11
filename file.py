08e675f21715b66630b50ef132f2741e22b35fe0
Kunjera03 <skkunjera004@gmail.com>
2025-07-11 09:43:33 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,3 +1,16 @@
+#Code1
+age = int(input("enter age : "))
+
+if(age >= 18):
+ if(age >= 80):
+    print("cannot drive")
+ else:
+    print("can drive")
+
+else:
+ print("cannot drive")
+
+#Code2
 def check():
     if(1 < 7):
         return True
@@ -9,3 +22,4 @@ def check():
         return True 
 
 check(7)
+ 
